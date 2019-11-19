03e7e180f526d30b71db9977d619ee00ebaceccf
Cy Weisman <cyweisman@gmail.com>
2019-11-19 15:57:05 -0600

    learned state, router

---
+++ src/Components/AddPhoto.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react'
+
+class AddPhoto extends Component {
+
+  constructor() {
+    super()
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    const imageLink = event.target.elements.link.value
+    const description = event.target.elements.description.value
+    const post = {
+      id: 0,
+      description: description,
+      imageLink: imageLink
+    }
+    if (description && imageLink){
+      this.props.onAddPhoto(post)
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Photwal </h1>
+        <div className="form">
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Link" name="link"/>
+            <input type="text" placeholder="Decri[]" name="description"/>
+            <button> Post </button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AddPhoto
