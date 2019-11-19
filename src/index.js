9a9cedd4c00a280c73d94b03e4e6fddff6b72684
Cy Weisman <cyweisman@gmail.com>
2019-11-19 10:46:51 -0600

    delete default files except index.js; learn elements

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const tasks = ['take out the trask', 'shovel drivway', 'walk dog'];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const element = React.createElement('ol', null,
+  tasks.map((task) => React.createElement('li', null, task))
+);
+
+ReactDOM.render(element, document.getElementById('root'));
